bedae82921ad802156f5c8a7c5acc9b1a2a81d68
Sergio Lema <sergio.lema.e@ucb.edu.bo>
2026-02-22 11:04:40 -0400

    Correcion archivo auth.middleware para borrar variable no usada

--- backend/academic-service/src/middleware/auth.middleware.js
+++ backend/academic-service/src/middleware/auth.middleware.js
@@ -13,7 +13,7 @@ module.exports = function authMiddleware(req, res, next) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // 🔑 CLAVE
     next();
-  } catch (err) {
+  } catch {
     return res.status(401).json({ error: "Token inválido" });
   }
 };
